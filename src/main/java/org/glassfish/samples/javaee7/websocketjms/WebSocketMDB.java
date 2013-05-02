ccc67d7ff08c939ddaa48dce90907765b1e664ea
Bruno Borges <bruno.borges@gmail.com>
2013-05-02 17:48:30 -0300

    fixed javadoc formatting

--- src/main/java/org/glassfish/samples/javaee7/websocketjms/WebSocketMDB.java
+++ src/main/java/org/glassfish/samples/javaee7/websocketjms/WebSocketMDB.java
@@ -1,17 +1,17 @@
 /**
- * Copyright © 2013, 2013, Oracle and/or its affiliates. All rights reserved. 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Copyright © 2013, 2013, Oracle and/or its affiliates. All rights reserved.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.glassfish.samples.javaee7.websocketjms;
 
@@ -27,8 +27,7 @@ import javax.jms.JMSException;
 
 /**
  * This MDB will fire CDI events with the JMS payload, classified as
- *
- * @WSJMSMessage
+ * <code>@WSJMSMessage</code>
  *
  * @author Bruno Borges <bruno.borges at oracle.com>
  */
